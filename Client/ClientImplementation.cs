c78858973487f1af75ad8e6826acc20ec90b7a73
Diogo Rato <diogo.rato.11@gmail.com>
2015-04-27 16:35:00 +0100

    Created RegisterNewWorker

--- Client/ClientImplementation.cs
+++ Client/ClientImplementation.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PADIMapNoReduce;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.IO;
+using System.Net.Sockets;
 
 namespace Client
 {
     class ClientImplementation : MarshalByRefObject, IClient{
 
-
         bool Submit(string inputFilePath, string outputDirectoryPath, string classImplementationPath, int numberOfSplits){
             
+            //Send Class Implementation for workers
+            string classMapperName = Path.GetFileNameWithoutExtension(classImplementationPath);
+            
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel, true);
+
+            IWorker mt = (IWorker)Activator.GetObject(typeof(IWorker), "tcp://localhost:10000/W");
+            try {
+                byte[] code = File.ReadAllBytes(classImplementationPath);
+                mt.SendMapper(code, classMapperName);
+            } catch (SocketException) {
+                System.Console.WriteLine("Could not locate server");
+            }
+
+            return true;
 
         }
 
-        string getInputSplit(int workerId, int inputBeginIndex, int inputEndIndex)
-        {
+        string getInputSplit(int workerId, int inputBeginIndex, int inputEndIndex){
             
         }
 
--- Worker/Program.cs
+++ Worker/Program.cs
@@ -6,45 +6,86 @@ using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 
 namespace PADIMapNoReduce {
-    /// <summary>
-    /// Program class is a container for application entry point Main
-    /// </summary>
+
     class Program {
+
         /// <summary>
         /// Application entry point Main
         /// </summary>
-        /// <param name="args">No required arguments</param>
+        /// <param name="args">ID SERVICE-URL ENTRY-URL</param>
+        ///
         static void Main(string[] args) {
-            TcpChannel channel = new TcpChannel(10000);
+
+            Uri serviceUri = new Uri(args[1]);
+
+            int tcpPort = serviceUri.Port;
+
+            //Create Service on this work
+            TcpChannel channel = new TcpChannel(tcpPort);
 
             ChannelServices.RegisterChannel(channel, true);
-            RemotingConfiguration.RegisterWellKnownServiceType(
-                typeof(WorkerServices),
-                "Worker",
-                WellKnownObjectMode.Singleton);
+            RemotingConfiguration.RegisterWellKnownServiceType( typeof(WorkerServices), "Worker", WellKnownObjectMode.Singleton);
+
+            //Inform JobTracker of this new worker 
+            Uri jobTrackerUri;
+
+            if (args[2] != null) {
+                
+                jobTrackerUri = new Uri(args[2]);
+
+            } else{
+
+                jobTrackerUri = new Uri(args[1]);
+
+            }
+
+                TcpChannel jobTrackerChannel = new TcpChannel();
+                ChannelServices.RegisterChannel(jobTrackerChannel, true);
+
+                IWorker mt = (IWorker)Activator.GetObject(typeof(IWorker), args[2]);
+
+                try{
+
+                    mt.RegisterNewWorker(args[1], true);
+                }
+                catch (SocketException)
+                {
+                    System.Console.WriteLine("Could not locate server");
+                }
+
+            
+
             System.Console.WriteLine("Press <enter> to terminate server...");
             System.Console.ReadLine();
+
         }
     }
 
 
     internal class WorkerServices : MarshalByRefObject, IWorker {
+
+        private byte[] code;
+        private string className;
+
+        private List<string> workersUrl;
+
         public bool SendMapper(byte[] code, string className) {
+
             Assembly assembly = Assembly.Load(code);
+
             // Walk through each type in the assembly looking for our class
             foreach (Type type in assembly.GetTypes()) {
+
                 if (type.IsClass == true) {
+
                     if (type.FullName.EndsWith("." + className)) {
+
                         // create an instance of the object
                         object ClassObj = Activator.CreateInstance(type);
 
                         // Dynamically Invoke the method
                         object[] args = new object[] { "testValue" };
-                        object resultObject = type.InvokeMember("Map",
-                          BindingFlags.Default | BindingFlags.InvokeMethod,
-                               null,
-                               ClassObj,
-                               args);
+                        object resultObject = type.InvokeMember("Map", BindingFlags.Default | BindingFlags.InvokeMethod, null, ClassObj, args);
                         IList<KeyValuePair<string, string>> result = (IList<KeyValuePair<string, string>>) resultObject;
                         Console.WriteLine("Map call result was: ");
                         foreach (KeyValuePair<string, string> p in result) {
@@ -57,5 +98,49 @@ namespace PADIMapNoReduce {
             throw (new System.Exception("could not invoke method"));
             return true;
         }
+
+        public bool RegisterNewWorker(List<string> workersServiceUrl, bool toBroadcast){
+
+            //Add to myself 
+            workersUrl.AddRange(workersServiceUrl);
+
+            if (toBroadcast) { //Resend to new worker ther whole workers list
+
+                foreach (string worker in workersUrl) {
+
+                    if (worker == workersServiceUrl[0]) {
+
+                        TcpChannel workerChannel = new TcpChannel();
+                        ChannelServices.RegisterChannel(workerChannel, true);
+
+                        IWorker mt = (IWorker)Activator.GetObject(typeof(IWorker), worker);
+
+                        try {
+                            mt.RegisterNewWorker(workersUrl, false);
+                        
+                        } catch (SocketException) {
+                        
+                            System.Console.WriteLine("[REGISTERWORKER1] Could not locate server");
+                        }
+
+                    } else { 
+
+                        TcpChannel workerChannel = new TcpChannel();
+                        ChannelServices.RegisterChannel(workerChannel, true);
+
+                        IWorker mt = (IWorker)Activator.GetObject(typeof(IWorker), worker);
+
+                        try {
+                            mt.RegisterNewWorker(workersServiceUrl[0], false);
+                        
+                        } catch (SocketException) {
+                        
+                            System.Console.WriteLine("[REGISTERWORKER2] Could not locate server");
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
