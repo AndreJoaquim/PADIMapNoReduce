767571135a279627696d5e84341bc3ef752cc004
AndreJoaquim <andrej_21@hotmail.com>
2015-04-29 16:00:48 +0100

    Changed UserApp and Client

--- Client/ClientImplementation.cs
+++ Client/ClientImplementation.cs
@@ -7,10 +7,36 @@ using System.Runtime.Remoting.Channels.Tcp;
 using System.Runtime.Remoting.Channels;
 using System.IO;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace Client
 {
-    class ClientImplementation : MarshalByRefObject, IClient{
+    class ClientImplementation : MarshalByRefObject, IClient {
+
+        private String url;
+
+        public ClientImplementation(){
+
+            // Get the IP's host
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    url = ip.ToString();
+                }
+            }
+
+            // Prepend the protocol and append the port
+            url = "tcp://" + url + ":" + NextFreeTcpPort() + "/C";
+
+            // Console message
+            Console.WriteLine("Created Client at " + url + ".");
+
+
+        }
 
         public bool Submit(string entryUrl, string inputFilePath, string outputDirectoryPath, string className, string classImplementationPath, int numberOfSplits){
             
@@ -39,7 +65,7 @@ namespace Client
 
         }
 
-        public string getInputSplit(int workerId, int inputBeginIndex, int inputEndIndex)
+        public string getInputSplit(int workerId, long inputBeginIndex, long inputEndIndex)
         {
             return "";
         }
@@ -47,6 +73,34 @@ namespace Client
         public bool sendProcessedSplit(int workerID, IList<KeyValuePair<string, string>> result)
         {
             return false;
+        }
+
+        /* 
+         * Utility functions
+         */
+        private int NextFreeTcpPort()
+        {
+
+            int portStartIndex = 10001;
+            int portEndIndex = 19999;
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpPoints = properties.GetActiveTcpListeners();
+
+            List<int> usedPorts = tcpPoints.Select(p => p.Port).ToList<int>();
+            int unusedPort = 0;
+
+            for (int port = portStartIndex; port <= portEndIndex; port++)
+            {
+                if (!usedPorts.Contains(port))
+                {
+                    unusedPort = port;
+                    break;
+                }
+            }
+
+            return unusedPort;
+
         }
     }
 }
